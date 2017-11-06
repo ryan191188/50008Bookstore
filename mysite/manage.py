7da250ddcdae1cf78f549907f9fca7733cea96d5
won soyoung <wonsoyoung@wonui-Air.dhcp.sutd.edu.sg>
2017-11-06 16:33:34 +0800

    adding django

---
+++ mysite/manage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
+
+
+from django.contrib.auth.models import User
+from django.contrib.auth.validators import ASCIIUsernameValidator
+
+class CustomUser(User):
+    username_validator = ASCIIUsernameValidator()
+    
+    class Meta:
+        proxy = True  # If no new field is added.
