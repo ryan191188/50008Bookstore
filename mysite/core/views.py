56ee0cf7e1440ab955f56ec2082eac7f1f2f42fd
yingjie <limyingjie@hotmail.com>
2017-11-08 16:40:17 +0800

    fixed typo

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-"from django.shortcuts import render
+#"from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth import login, authenticate
