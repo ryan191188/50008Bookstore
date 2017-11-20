2b7d45ed0ba2a888c5680ab936f1496a9c4f1c21
2ll2sij <2ll2sij@gmail.com>
2017-11-20 12:49:36 +0800

    Add files via upload

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,56 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+"""from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+"""
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from mysite import views as core_views
+
+
+urlpatterns = [
+               #url(r'^polls/', include('polls.urls')),
+               
+               
+               url(r'^accounts/login/',
+                   auth_views.login,
+                   {
+                   'template_name': 'login.html'
+                   },
+                   name='login'
+                   ,),
+               url(r'^accounts/logout/',
+                   auth_views.logout,
+                   {
+                   'template_name': 'logout.html'
+                   },
+                   name='logout'),
+               
+               url(r'^admin/', admin.site.urls),
+               
+               url(r'^accounts/signup/',
+                   core_views.signup, name='signup'),
+
+               url(r'^search/',
+                   core_views.search, name='search'),
+                              
+               ]
