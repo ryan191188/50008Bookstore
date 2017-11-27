cc55eed1d3627a9d1b66fdfec7b5ba6491c4564b
ryan191188 <ryan_zhong@mymail.sutd.edu.sg>
2017-11-27 18:59:04 +0800

    views error

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,27 +4,24 @@ from __future__ import unicode_literals
 #from django.shortcuts import render
 
 # Create your views here.
-from django.core.exceptions import PermissionDenied
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect, render_to_response
-from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
-
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-	print (form.is_valid())
-        if form.is_valid():
+    print (form.is_valid())
+    if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-	    messages.info(request, 'User created!')
+            messages.info(request, 'User created!')
             return redirect('/accounts/login') #Create a new page late
-	
+
     else:
         form = UserCreationForm()
     
@@ -37,50 +34,43 @@ def logout(request):
 def search(request):
 
     for key in request.GET.keys():
-        print key,":",request.GET[key]
+        print (key,":",request.GET[key])
     # insert SQL query here
     context = {"results": (('Photoshop Elements 9: The Missing Manual', 'paperback', '640', 'English', 'Barbara Brundage', 'Pogue Press', 'Science', '2010', '1449389678', '978-1449389673', 40),('Where Good Ideas Come From: The Natural History of Innovation', 'hardcover', '336', 'English', 'Steven Johnson', 'Riverhead Hardcover', 'Biology', '2010', '1594487715', '978-1594487712', 46))} #example results
     return render(request, 'search.html', context)
 
 def orders(request):
-	args = {}
-	#args.update(csrf(request))
-	#args['error'] = ""
-	#args['ProductOrderForm'] = ProductOrderForm()
+    args = {}
+    #args.update(csrf(request))
+    #args['error'] = ""
+    #args['ProductOrderForm'] = ProductOrderForm()
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		order = Order(user=request.user.storeuser, paid=False)
-		order.save()
-		product_order = ProductOrder(order=order)
-		form = ProductOrderForm(request.POST, instance=product_order)
+        order = Order(user=request.user.storeuser, paid=False)
+        order.save()
+        product_order = ProductOrder(order=order)
+        form = ProductOrderForm(request.POST, instance=product_order)
+        if form.is_valid():
+            form.save()
+            args['order_id'] = order.pk
+            return render(request, 'store/orders_more.html', args)
+        else:
+            order.delete()
+            args['error'] = "Order Submission Failed!"
+            render(request, 'store/order_form.html', args)
 
-		if form.is_valid():
-			form.save()
-			args['order_id'] = order.pk
-			return render(request, 'store/orders_more.html', args)
-		else:
-			order.delete()
-			args['error'] = "Order Submission Failed!"
-			render(request, 'store/order_form.html', args)
-
-	return render(request, 'store/order_form.html', args)
+    return render(request, 'store/order_form.html', args)
 
 def books(request):
-	args = {'book':('Photoshop Elements 9: The Missing Manual', 'paperback', '640', 'English', 'Barbara Brundage', 'Pogue Press', 'Science', '2010', '1449389678', '978-1449389673', 40)}  #tuple that contains info on all the books. REPLACE THE TUPLE WITH A QUERY LANGUAGE TO GET THE BOOK. SHOULD GET THE ROWS
+    args = {'book':('Photoshop Elements 9: The Missing Manual', 'paperback', '640', 'English', 'Barbara Brundage', 'Pogue Press', 'Science', '2010', '1449389678', '978-1449389673', 40)}  #tuple that contains info on all the books. REPLACE THE TUPLE WITH A QUERY LANGUAGE TO GET THE BOOK. SHOULD GET THE ROWS
 #NEED TO DO HTML FOR 'book' TO DO BOOK DETAILS
-	print (request.path).split('/')[2]  ##this is the ISBN13 number used to query
-	return render(request, 'books/book_details.html',args)
+    print (request.path).split('/')[2]  ##this is the ISBN13 number used to query
+    return render(request, 'books/book_details.html',args)
 
-@login_required
 def userdata(request):
-    if not request.user.username==request.path.split('/')[2]:
-        raise PermissionDenied('NOT LOGGED IN')
-    args={}
-    return render(request, 'users.html',args)
+    print (request.user.username)
 
-
-	
 
 
 
